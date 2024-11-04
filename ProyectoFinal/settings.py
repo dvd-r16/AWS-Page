e1a5bc08a1eb7ff9fc8fc1297f2fb606d623285e
dvd-r16 <3711111370101@ingenieria.usac.edu.gt>
2024-11-04 04:58:52 -0600

    Update settings.py - DOMAIN ADDED

--- ProyectoFinal/settings.py
+++ ProyectoFinal/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-wqk%(e0g0=h^20g^(n$ay&0ry$iv+q6eprq*2dzxyl0p61!n03
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.224.99.121', 'localhost', 'clinicaserver-1939774371.us-east-1.elb.amazonaws.com']
+ALLOWED_HOSTS = ['54.224.99.121', 'localhost', 'clinicaserver-1939774371.us-east-1.elb.amazonaws.com', 'sonrisasdelavida.click']
 
 #STATICFILES_DIRS = ['D:/ウサク/2022/Segundo Semestre/Proyectos de computación/Proyecto/Proyecto Final/ProyectoFinal/Templates/static']
 
